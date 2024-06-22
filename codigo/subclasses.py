fc67efe424876700c0f1070d6603023d1965ddf9
Davi <davirobergemachado@gmail.com>
2024-06-22 14:00:43 -0300

    correcao goleiro

--- codigo/subclasses.py
+++ codigo/subclasses.py
@@ -5,8 +5,6 @@ class Goleiro(Jogador):
   def __init__(self, nome_jogador, numero_jogador, pos_jogador, habgoleiro, defesa, fisico, passe, drible, velocidade, finalizacao):
     super().__init__(nome_jogador, numero_jogador, pos_jogador, habgoleiro, defesa, fisico, passe, drible, velocidade, finalizacao)
 
-    self.habgoleiro = habgoleiro*10
-
     overall = habgoleiro
 
     self.overall = Decimal(overall).quantize(0, ROUND_HALF_UP)
