b6c6fc40a35763589dcf8b7ba50b4eb024a9cb4b
Davi <davirobergemachado@gmail.com>
2024-06-25 23:26:07 -0300

    fator aleatorio, mas com erro nos resultados #8

--- codigo/classes.py
+++ codigo/classes.py
@@ -1,7 +1,12 @@
 from decimal import Decimal, ROUND_HALF_UP
+import random
 
 class Time:
     def __init__(self, nome_time, lista_jogadores, lista_goleiros, lista_defensores, lista_meio_campistas, lista_atacantes):
+
+        # Define atributo perdedor
+        perdedor = False
+        self.perdedor = perdedor
 
         # Define atributos recebidos
         self.nome_time = nome_time
@@ -82,17 +87,29 @@ class Time:
     def get_passes(self):
         return self.passes
     
+    def get_perdedor(self):
+        return self.perdedor
+    
+    def set_perdedor(self):
+        self.perdedor = True
+
+    def set_indice_ofensivo(self, aleatorio):
+        io = float(self.indidice_ofensivo)*aleatorio
+        self.indidice_ofensivo = Decimal(io).quantize(0, ROUND_HALF_UP)
+    
 class Jogador:
     def __init__(self, nome_jogador, numero_jogador, pos_jogador, habgoleiro, defesa, fisico, passe, drible, velocidade, finalizacao):
         partidas_jogadas = 0
         partidas_vencidas = 0
         gols_marcadados = 0
         assistencias_feitas = 0
+        is_capitao = False
 
         self.partidas_jogadas = partidas_jogadas
         self.partidas_vencidas = partidas_vencidas
         self.gols_marcados = gols_marcadados
         self.assistencias_feitas = assistencias_feitas
+        self.is_capitao = is_capitao
         self.pos_jogador = pos_jogador
         self.nome_jogador = nome_jogador
         self.numero_jogador = numero_jogador
@@ -116,25 +133,116 @@ class Jogador:
     def get_assistencias_feitas(self):
         return self.assistencias_feitas
     
-        
+    def set_is_capitao(self):
+        self.is_capitao = True
+    
 
 class Jogo:
-    def __init__(self, gols_time1, gols_time2, time1, time2):
+    def __init__(self, time1, time2):
         self.time1 = time1
         self.time2 = time2
-        self.gols_time1 = gols_time1
-        self.gols_time2 = gols_time2
+    
+    def simulacao(self):
+        for player1 in self.time1.lista_jogadores:
+            player1.partidas_jogadas += 1
+
+        for player2 in self.time2.lista_jogadores:
+            player2.partidas_jogadas += 1
+
+        gols1 = (self.time1.get_indice_ofensivo()-self.time2.get_indice_defensivo())
+
+        if (gols1 < 0):
+            gols1 = 0
+
+        gols2 = (self.time2.get_indice_ofensivo()-self.time1.get_indice_defensivo())
+
+        if (gols2 < 0):
+            gols2 = 0
+
+        self.gols1 = gols1
+        self.gols2 = gols2
+
+
+        print(f"Indice ofensivo time 1 {self.time1.get_indice_ofensivo()}")
+        print(f"Indice defensivo time 1 {self.time1.get_indice_defensivo()}")
+        print(f"Indice ofensivo time 2 {self.time2.get_indice_ofensivo()}")
+        print(f"Indice defensivo time 2 {self.time2.get_indice_defensivo()}")
+
+    def resultado(self):
+        print(f"Resultado do jogo:\n{self.time1.get_nome_time()}  {self.gols1} X {self.gols2}  {self.time2.get_nome_time()}")
+
+        self.gols1 = int(self.gols1)
+        self.gols2 = int(self.gols2)
+
+        if (self.gols1 > 0):
+            artilheiros1 = []
+            arti1 = random.choices(self.time1.lista_jogadores, weights = self.time1.finalizacoes, k = self.gols1)
+
+            for art1 in arti1:
+                art1.gols_marcados += 1
+                artilheiros1.append(art1.get_nome())
+
+            assistentes1 = []
+            assis1 = random.choices(self.time1.lista_jogadores, weights = self.time1.passes, k = self.gols1)
+
+            for assi1 in assis1:
+                assi1.assistencias_feitas += 1
+                assistentes1.append(assi1.get_nome())
+
+            for f1 in range(self.gols1):
+                if (assistentes1[f1] == artilheiros1[f1]):
+                    assis1[f1].assistencias_feitas -= 1
+                    assistentes1.pop(f1)
+
+            print(f"Os gols do time {self.time1.get_nome_time()} foram de {artilheiros1}")
+            print(f"As assistências do time {self.time1.get_nome_time()} foram de {assistentes1}")
+
+        if (self.gols2 > 0):
+            artilheiros2 = []
+            arti2 = random.choices(self.time2.lista_jogadores, weights = self.time2.finalizacoes, k = self.gols2)
+
+            for art2 in arti2:
+                art2.gols_marcados += 1
+                artilheiros2.append(art2.get_nome())
+
+            assistentes2 = []
+            assis2 = random.choices(self.time2.lista_jogadores, weights = self.time2.passes, k = self.gols2)
+
+            for assi2 in assis2:
+                assi2.assistencias_feitas += 1
+                assistentes2.append(assi2.get_nome())
+
+            for f2 in range(self.gols2):
+                if (assistentes1[f2] == artilheiros1[f2]):
+                    assis2[f2].assistencias_feitas -= 1
+                    assistentes1.pop(f2)
+
+            print(f"Os gols do time {self.time2.get_nome_time()} foram de {artilheiros2}")
+            print(f"As assistências do time {self.time2.get_nome_time()} foram de {assistentes2}")
+
+    def perdedor(self):
+        if(self.gols1 > self.gols2):
+            self.time2.set_perdedor()
+        elif(self.gols1 < self.gols2):
+            self.time1.set_perdedor()
+        else:
+            self.time1.set_perdedor()
+            self.time2.set_perdedor()
+    
+    def zebra(self):
+        if(self.time1.get_perdedor() == True):
+            aleatorio = random.uniform(1, 1.5)
+            self.time1.set_indice_ofensivo(aleatorio)
+        if(self.time2.get_perdedor() == True):
+            aleatorio = random.uniform(1, 1.5)
+            self.time2.set_indice_ofensivo(aleatorio)
 
     def ganhador(self):
-        if(self.gols_time1 > self.gols_time2):
+        if(self.gols1 > self.gols2):
             for player1 in self.time1.lista_jogadores:
                 player1.partidas_vencidas += 1
-        elif(self.gols_time1 < self.gols_time2):
+        elif(self.gols1 < self.gols2):
             for player2 in self.time2.lista_jogadores:
                 player2.partidas_vencidas += 1
-
-    def get_gols_time1(self):
-        return self.gols_time1
-    
-    def get_gols_time2(self):
-        return self.gols_time2
+        self.time1.set_perdedor()
+        self.time1.set_perdedor()
--- codigo/teste.py
+++ codigo/teste.py
@@ -1,111 +1,118 @@
-import random
 from classes import Jogo
 from time1 import time1
 from time2 import time2
 
-def simulacao(time1, time2):
-    for player1 in time1.lista_jogadores:
-        player1.partidas_jogadas += 1
+jogo = Jogo(time1, time2)
+jogo.simulacao()
+jogo.perdedor()
+jogo.zebra()
+jogo.simulacao()
+jogo.resultado()
+jogo.ganhador()
 
-    for player2 in time2.lista_jogadores:
-        player2.partidas_jogadas += 1
+# def simulacao(time1, time2):
+#     for player1 in time1.lista_jogadores:
+#         player1.partidas_jogadas += 1
 
-    gols1 = (time1.get_indice_ofensivo()-time2.get_indice_defensivo())
+#     for player2 in time2.lista_jogadores:
+#         player2.partidas_jogadas += 1
 
-    if (gols1 < 0):
-        gols1 = 0
+#     gols1 = (time1.get_indice_ofensivo()-time2.get_indice_defensivo())
 
-    gols2 = (time2.get_indice_ofensivo()-time1.get_indice_defensivo())
+#     if (gols1 < 0):
+#         gols1 = 0
 
-    if (gols2 < 0):
-        gols2 = 0
+#     gols2 = (time2.get_indice_ofensivo()-time1.get_indice_defensivo())
 
-    jogo = Jogo(gols1, gols2, time1, time2)
-    jogo.ganhador()
+#     if (gols2 < 0):
+#         gols2 = 0
 
-    print(f"Resultado do jogo:\n{time1.get_nome_time()}  {jogo.get_gols_time1()} X {jogo.get_gols_time2()}  {time2.get_nome_time()}")
+#     jogo = Jogo(gols1, gols2, time1, time2)
+#     jogo.ganhador()
 
-    gols1 = int(gols1)
-    gols2 = int(gols2)
+#     print(f"Resultado do jogo:\n{time1.get_nome_time()}  {jogo.get_gols_time1()} X {jogo.get_gols_time2()}  {time2.get_nome_time()}")
 
-    if (gols1 > 0):
-        artilheiros1 = []
-        arti1 = random.choices(time1.lista_jogadores, weights = time1.finalizacoes, k = gols1)
+#     gols1 = int(gols1)
+#     gols2 = int(gols2)
 
-        for art1 in arti1:
-            art1.gols_marcados += 1
-            artilheiros1.append(art1.get_nome())
+#     if (gols1 > 0):
+#         artilheiros1 = []
+#         arti1 = random.choices(time1.lista_jogadores, weights = time1.finalizacoes, k = gols1)
 
-        assistentes1 = []
-        assis1 = random.choices(time1.lista_jogadores, weights = time1.passes, k = gols1)
+#         for art1 in arti1:
+#             art1.gols_marcados += 1
+#             artilheiros1.append(art1.get_nome())
 
-        for assi1 in assis1:
-            assi1.assistencias_feitas += 1
-            assistentes1.append(assi1.get_nome())
+#         assistentes1 = []
+#         assis1 = random.choices(time1.lista_jogadores, weights = time1.passes, k = gols1)
 
-        for f1 in range(gols1):
-            if (assistentes1[f1] == artilheiros1[f1]):
-                assis1[f1].assistencias_feitas -= 1
-                assistentes1.pop(f1)
+#         for assi1 in assis1:
+#             assi1.assistencias_feitas += 1
+#             assistentes1.append(assi1.get_nome())
 
-        print(f"Os gols do time {time1.get_nome_time()} foram de {artilheiros1}")
-        print(f"As assistências do time {time1.get_nome_time()} foram de {assistentes1}")
+#         for f1 in range(gols1):
+#             if (assistentes1[f1] == artilheiros1[f1]):
+#                 assis1[f1].assistencias_feitas -= 1
+#                 assistentes1.pop(f1)
 
-    if (gols2 > 0):
-        artilheiros2 = []
-        arti2 = random.choices(time2.lista_jogadores, weights = time2.finalizacoes, k = gols2)
+#         print(f"Os gols do time {time1.get_nome_time()} foram de {artilheiros1}")
+#         print(f"As assistências do time {time1.get_nome_time()} foram de {assistentes1}")
 
-        for art2 in arti2:
-            art2.gols_marcados += 1
-            artilheiros2.append(art2.get_nome())
+#     if (gols2 > 0):
+#         artilheiros2 = []
+#         arti2 = random.choices(time2.lista_jogadores, weights = time2.finalizacoes, k = gols2)
 
-        assistentes2 = []
-        assis2 = random.choices(time2.lista_jogadores, weights = time2.passes, k = gols2)
+#         for art2 in arti2:
+#             art2.gols_marcados += 1
+#             artilheiros2.append(art2.get_nome())
 
-        for assi2 in assis2:
-            assi2.assistencias_feitas += 1
-            assistentes2.append(assi2.get_nome())
+#         assistentes2 = []
+#         assis2 = random.choices(time2.lista_jogadores, weights = time2.passes, k = gols2)
 
-        for f2 in range(gols2):
-            if (assistentes1[f2] == artilheiros1[f2]):
-                assis2[f2].assistencias_feitas -= 1
-                assistentes1.pop(f2)
+#         for assi2 in assis2:
+#             assi2.assistencias_feitas += 1
+#             assistentes2.append(assi2.get_nome())
 
-        print(f"Os gols do time {time2.get_nome_time()} foram de {artilheiros2}")
-        print(f"As assistências do time {time2.get_nome_time()} foram de {assistentes2}")
+#         for f2 in range(gols2):
+#             if (assistentes1[f2] == artilheiros1[f2]):
+#                 assis2[f2].assistencias_feitas -= 1
+#                 assistentes1.pop(f2)
+
+#         print(f"Os gols do time {time2.get_nome_time()} foram de {artilheiros2}")
+#         print(f"As assistências do time {time2.get_nome_time()} foram de {assistentes2}")
 
     
 
-def consulta(time1, time2):
-    print(f"{time1.get_nome_time()} - {time1.get_overall_time()}:\n")
-    for i in range (11):
-        print(f"{time1.lista_jogadores[i].get_pos()}: {time1.lista_jogadores[i].get_numero()} - {time1.lista_jogadores[i].get_nome()} - OVR: {time1.lista_jogadores[i].get_overall()}")
+# def consulta(time1, time2):
+#     print(f"{time1.get_nome_time()} - {time1.get_overall_time()}:\n")
+#     for i in range (11):
+#         print(f"{time1.lista_jogadores[i].get_pos()}: {time1.lista_jogadores[i].get_numero()} - {time1.lista_jogadores[i].get_nome()} - OVR: {time1.lista_jogadores[i].get_overall()}")
 
-    print()
-    print(f"{time2.get_nome_time()} - {time2.get_overall_time()}:\n")
-    for i in range (11):
-        print(f"{time2.lista_jogadores[i].get_pos()}: {time2.lista_jogadores[i].get_numero()} - {time2.lista_jogadores[i].get_nome()} - OVR: {time2.lista_jogadores[i].get_overall()}")
+#     print()
+#     print(f"{time2.get_nome_time()} - {time2.get_overall_time()}:\n")
+#     for i in range (11):
+#         print(f"{time2.lista_jogadores[i].get_pos()}: {time2.lista_jogadores[i].get_numero()} - {time2.lista_jogadores[i].get_nome()} - OVR: {time2.lista_jogadores[i].get_overall()}")
 
-    print(f"Índice defensivo: {time1.get_indice_defensivo()} Índice ofensivo: {time1.get_indice_ofensivo()}")
-    print(f"Índice defensivo: {time2.get_indice_defensivo()} Índice ofensivo: {time2.get_indice_ofensivo()}")
+#     print(f"Índice defensivo: {time1.get_indice_defensivo()} Índice ofensivo: {time1.get_indice_ofensivo()}")
+#     print(f"Índice defensivo: {time2.get_indice_defensivo()} Índice ofensivo: {time2.get_indice_ofensivo()}")
 
 
-for r in range(5):
-    consulta(time1, time2)
-    simulacao(time1, time2)
+# for r in range(5):
+#     consulta(time1, time2)
+#     simulacao(time1, time2)
 
-for p1 in time1.lista_jogadores:
-    print(p1.get_nome())
-    print(p1.get_partidas_jogadas())
-    print(p1.get_partidas_vencidas())
-    print(p1.get_gols_marcados())
-    print(p1.get_assistencias_feitas())
+# for p1 in time1.lista_jogadores:
+#     print(f"Nome: {p1.get_nome()}")
+#     print(f"Partidas jogadas: {p1.get_partidas_jogadas()}")
+#     print(f"Partidas vencidas: {p1.get_partidas_vencidas()}")
+#     print(f"Gols marcados: {p1.get_gols_marcados()}")
+#     print(f"Assistências feitas: {p1.get_assistencias_feitas()}")
 
-for p2 in time2.lista_jogadores:
-    print(p2.get_nome())
-    print(p2.get_partidas_jogadas())
-    print(p2.get_partidas_vencidas())
-    print(p2.get_gols_marcados())
-    print(p2.get_assistencias_feitas())
+# for p2 in time2.lista_jogadores:
+#     print(f"Nome: {p2.get_nome()}")
+#     print(f"Partidas jogadas: {p2.get_partidas_jogadas()}")
+#     print(f"Partidas vencidas: {p2.get_partidas_vencidas()}")
+#     print(f"Gols marcados: {p2.get_gols_marcados()}")
+#     print(f"Assistências feitas: {p2.get_assistencias_feitas()}")
 
 
